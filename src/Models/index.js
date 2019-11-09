6b4461dcb7fb9094386628232cdaf020ee938cef
kiran-kumar011 <kirankirankumar56@gmail.com>
2019-11-09 18:04:48 +0530

    keeping one source for rendering Input

---
+++ src/Models/index.js
@@ -0,0 +1,28 @@
+const formModel = [{
+	type: 'text',
+	name: 'name',
+	label: 'Name',
+	placeholder: 'Enter your name',
+	width: '100%'
+}, {
+	type: 'text',
+	name: 'email',
+	label: 'Email',
+	placeholder: 'Enter your email',
+	width: '100%'
+}, {
+	type: 'number',
+	label: 'Mobile Number', 
+	name: 'phoneNumber',
+	placeholder: 'Enter your mobile number',
+	width: '100%'
+}, {
+	type: 'password',
+	label: 'Password',
+	name: 'password',
+	placeholder: 'Create your password',
+	width: '100%'
+}]
+
+
+export default formModel;
